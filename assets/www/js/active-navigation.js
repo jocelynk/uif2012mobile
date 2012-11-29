8f4c4d92f0813e3f1769b0a151c98f66216f0116
aakintil <aakintil@andrew.cmu.edu>
2012-11-29 16:08:17 -0500

    adding transition for checkin page

--- assets/www/js/active-navigation.js
+++ assets/www/js/active-navigation.js
@@ -13,10 +13,25 @@ $(document).ready(function()
             $(this).removeClass("active");  
         }) 
     })
-    
+
     //reset type=date inputs to text
     $( document ).bind( "mobileinit", function(){
-      $.mobile.page.prototype.options.degradeInputs.date = true;
+        $.mobile.page.prototype.options.degradeInputs.date = true;
     });
-    
-}); 
+
+
+    $("#date").bind('change', function()
+    {
+        console.log($("input").val()); 
+        if ($(this).val().length > 1)
+        {
+            $("#checkin .hidden").removeClass('hidden').addClass('visible'); 
+        }
+        else
+        {
+            $("#checkin .visible").removeClass('visible').addClass('hidden');
+        }
+    }); 
+
+
+});

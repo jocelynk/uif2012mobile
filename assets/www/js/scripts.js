a63c5272523a13821005c4b5b54ef47797e9d7cf
jocelynk <jocelynk@andrew.cmu.edu>
2012-12-02 17:40:45 -0500

    making more responsive design, issue with input and links can't click, and need to fix more for mobile

---
+++ assets/www/js/scripts.js
@@ -0,0 +1,76 @@
+/*
+* jquery.dropdown.js
+* Apply sexy dropdowns on any ul with child ul.
+* Depends on: jquery.hoverIntent.js
+*/
+
+$.fn.dropdown = function(options) {
+var defaults = {};
+  var opts = $.extend(defaults, options);
+
+  // Apply on those items with children
+  this.each(function() {
+    $(this).find('li').each(function() {
+      if($(this).find("ul").length > 0) {
+        $(this).addClass("hasChildren");
+        $(this).find('> a').wrapInner("<span></span>");
+      }
+    });
+  });
+
+  // Apply on all list items
+  $(this).find("li").on('hover', function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+};
+
+$(function() {
+
+  // Calling the jquery dropdown
+  $('nav').dropdown();
+  /*
+  * Applicable only to Mobile-nav
+  */
+
+  // Checking if the top-right button is visible.
+  if ($("nav a.btn-navbar").is(":visible")) {
+
+    // Making the dropdown magically appear onclick/touch.
+    
+    $('nav a.btn-navbar').on('click', function() {
+      $('ul#menu').slideToggle('fast', function() {
+        $('ul#menu').css({
+
+        // The height must be fixed for the native-scrolling on iOS
+        'height': $(this).height(),
+
+        // But we don't want the height of the nav to exceed the viewport.
+        'max-height': $(window).height() + 20
+        });
+      });
+
+      $(this).toggleClass('active');
+
+    }); // end on.click
+
+    // Making the children appear on click/touch
+    $('ul#menu li.hasChildren a').on('click', function() {
+      $(this).parent().children('ul').slideToggle('fast', function() {
+
+          $('ul#menu').css({
+
+          // Resetting the height to auto in order to expand/contract the menu upon interaction.
+          'height': 'auto',
+
+          // But we don't want the height of the nav to exceed the viewport.
+          'max-height': $(window).height() + 20
+          });
+
+        }); // end slideToggle
+    }); // end on.click
+
+ } // end visibility check
+
+});

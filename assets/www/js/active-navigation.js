74ca84a8107d4ae858d67b7a1215759aea6b42eb
jocelynk <jocelynk@andrew.cmu.edu>
2012-12-12 17:32:01 -0500

    merging designs

--- assets/www/js/active-navigation.js
+++ assets/www/js/active-navigation.js
@@ -10,18 +10,22 @@ $(document).ready(function () {
         });
 
 
-   $('.datescroller').mobiscroll({
-        preset: 'date',
-        invalid: { daysOfWeek: [0, 6], daysOfMonth: ['5/1', '12/24', '12/25'] },
-        theme: 'default',
-        display: 'modal',
-        mode: 'scroller',
-        animate: 'fade',
-        dateOrder: 'yy-mm-dd'
-    });
-
+     // tactile feedback / button effects
+   
+   $("#addIcon").bind("touchstart", function(e)
+   {
+       e.preventDefault();
+       $(this).addClass("pressed");
+   }); 
+   
+   $("#addIcon").bind("touchend", function(e)
+   {
+       e.preventDefault();
+       $(this).removeClass("pressed");  
+   });
+   
+   
   // for click devices
-  // why this works...i have no idea
   $("a.btn-navbar").bind('tapone', function (e) {
     e.preventDefault();
     //$("a.btn-navbar").click(function() {
